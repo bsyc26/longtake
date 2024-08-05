96924366798c7babe73dabffb8fc65f389d6f375
bsyc26 <bsyc26@gmail.com>
2024-08-06 00:25:46 +0800

    use Redux; add Message and Loader components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,29 +1,33 @@
-import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import Product from '../components/Product';
-import axios from 'axios';
+import Product from '../components/Product.jsx';
+import Loader from '../components/Loader.jsx';
+import Message from '../components/Message.jsx';
+import { useGetProductsQuery } from '../slices/productsApiSlice.js';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []); // init once
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+        // <h2>Loading...</h2>
+      ) : error ? (
+        <Message variant='danger'>
+          { error?.data?.message || error.error }
+        </Message>
+      ) : (
+      <>
+        <h1>Latest Products</h1>
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </>
+      )}
     </>
   );
 };
