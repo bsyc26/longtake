e009b80160672635351cee09a6eb181e11660598
bsyc26 <bsyc26@gmail.com>
2024-08-09 02:25:32 +0800

    create backend order APIs

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middlewares/errorHandler.js';
 
 const port = process.env.PORT || 5000;
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
