3a51ab8830ba500f1073981197aed24501cb4032
bsyc26 <bsyc26@gmail.com>
2024-08-20 23:13:44 +0800

    create production and build

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middlewares/errorHandler.js';
 
-const port = process.env.PORT || 5000;
-
 dotenv.config();
+
+const port = process.env.PORT || 5173;
 
 connectDB();
 
@@ -35,6 +35,20 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve(); // set __dirname to pwd
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // any route that is not api redirect to index.html
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
